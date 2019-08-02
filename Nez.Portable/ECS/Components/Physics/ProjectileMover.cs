034ee69f69b532609fda9589d34a4c0488ef3a10
Sander.M <witlock@outlook.com>
2019-08-02 21:55:25 +0300

    projectile mover multiple colliders

--- Nez.Portable/ECS/Components/Physics/ProjectileMover.cs
+++ Nez.Portable/ECS/Components/Physics/ProjectileMover.cs
@@ -10,62 +10,64 @@ namespace Nez
 	/// </summary>
 	public class ProjectileMover : Component
 	{
-		List<ITriggerListener> _tempTriggerList = new List<ITriggerListener>();
-		Collider _collider;
+        List<ITriggerListener> _tempTriggerList = new List<ITriggerListener>();
+        List<Collider> _colliders;
 
 
-		public override void onAddedToEntity()
-		{
-			_collider = entity.getComponent<Collider>();
-			Debug.warnIf( _collider == null, "ProjectileMover has no Collider. ProjectilMover requires a Collider!" );
-		}
+        public override void onAddedToEntity()
+        {
+            _colliders = entity.getComponents<Collider>();
+            Insist.isNotNull(_colliders, "null Collider. ProjectilMover requires a Collider!");
+        }
 
 
-		/// <summary>
-		/// moves the entity taking collisions into account
-		/// </summary>
-		/// <returns><c>true</c>, if move actor was newed, <c>false</c> otherwise.</returns>
-		/// <param name="motion">Motion.</param>
-		public bool move( Vector2 motion )
-		{
-			if( _collider == null )
-				return false;
+        /// <summary>
+        /// moves the entity taking collisions into account
+        /// </summary>
+        /// <returns><c>true</c>, if move actor was newed, <c>false</c> otherwise.</returns>
+        /// <param name="motion">Motion.</param>
+        public bool move(Vector2 motion)
+        {
+            var didCollide = false;
 
-			var didCollide = false;
+            // fetch anything that we might collide with at our new position
+            entity.transform.position += motion;
 
-			// fetch anything that we might collide with at our new position
-			entity.transform.position += motion;
+            foreach (var collider in _colliders)
+            {
 
-			// fetch anything that we might collide with us at our new position
-			var neighbors = Physics.boxcastBroadphase( _collider.bounds, _collider.collidesWithLayers );
-			foreach( var neighbor in neighbors )
-			{
-				if( _collider.overlaps( neighbor ) )
-				{
-					didCollide = true;
-					notifyTriggerListeners( _collider, neighbor );
-				}
-			}
+                // fetch anything that we might collide with us at our new position
+                var neighbors = Physics.boxcastBroadphase(collider.bounds, collider.collidesWithLayers);
+                foreach (var neighbor in neighbors)
+                {
+                    if (collider.overlaps(neighbor))
+                    {
+                        didCollide = true;
+                        notifyTriggerListeners(collider, neighbor);
+                    }
+                }
 
-			return didCollide;
-		}
+            }
 
-		void notifyTriggerListeners( Collider self, Collider other )
-		{
-			// notify any listeners on the Entity of the Collider that we overlapped
-			other.entity.getComponents( _tempTriggerList );
-			for( var i = 0; i < _tempTriggerList.Count; i++ )
-				_tempTriggerList[i].onTriggerEnter( self, other );
+            return didCollide;
+        }
 
-			_tempTriggerList.Clear();
 
-			// notify any listeners on this Entity
-			entity.getComponents( _tempTriggerList );
-			for( var i = 0; i < _tempTriggerList.Count; i++ )
-				_tempTriggerList[i].onTriggerEnter( other, self );
+        void notifyTriggerListeners(Collider self, Collider other)
+        {
+            // notify any listeners on the Entity of the Collider that we overlapped
+            other.entity.getComponents(_tempTriggerList);
+            for (var i = 0; i < _tempTriggerList.Count; i++)
+                _tempTriggerList[i].onTriggerEnter(self, other);
 
-			_tempTriggerList.Clear();
-		}
-	}
+            _tempTriggerList.Clear();
+
+            // notify any listeners on this Entity
+            entity.getComponents(_tempTriggerList);
+            for (var i = 0; i < _tempTriggerList.Count; i++)
+                _tempTriggerList[i].onTriggerEnter(other, self);
+
+            _tempTriggerList.Clear();
+        }
+    }
 }
-
